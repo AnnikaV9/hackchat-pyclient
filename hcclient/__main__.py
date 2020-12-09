5a02039fef67a52ae2e785003c3c827f054d2cf7
V9 <68383195+AnnikaV9@users.noreply.github.com>
2020-12-09 13:40:58 +0800

    Removed termcolor usage

--- hcclient/__main__.py
+++ hcclient/__main__.py
@@ -8,7 +8,6 @@ import threading
 import websocket
 import colorama
 from time import sleep
-from termcolor import colored
 
 
 
@@ -18,14 +17,17 @@ class Client:
      
  
        
-    def __init__(self, nick, channel="programming", target_websocket="wss://hack.chat/chat-ws"):  
+    def __init__(self, nick, password, channel="programming", target_websocket="wss://hack.chat/chat-ws"):  
         self.nick = nick
         self.channel = channel
         self.online_users = []
+        self.full_nick = "{}#{}".format(nick, password)
         self.ws = websocket.create_connection(target_websocket)
-        self.ws.send(json.dumps({"cmd": "join", "channel": channel, "nick": nick}))
+        self.ws.send(json.dumps({"cmd": "join", "channel": channel, "nick": self.full_nick}))
 
-        print("\n\nYou are now connected to the channel: {}".format(channel))
+        print("\n\nYou are now connected to the channel: {}".format(color_list.RED +
+                                                                    channel +
+                                                                    color_list.END))
         print("Type '/list' to see who's online.\n\n".format(self.online_users))
 
         self.thread_ping = threading.Thread(target=self.ping_thread)
@@ -35,36 +37,57 @@ class Client:
 
 
     def main_thread(self):        
-
+        you_text = color_list.RED + "you:" + color_list.END
+        
         while True:            
             received = json.loads(self.ws.recv())
             
             if received["cmd"] == "chat":
+                colored_nick = color_list.CYAN + received["nick"] + color_list.END
+
                 if received["nick"] != self.nick:
-                    print("{}|[{}] {}".format(colored(received.get("trip", ""), 'white'), colored(received["nick"], "cyan"), received["text"]))
-                else:
-                    print("{}{}|[{}] {}".format(colored("you:","red", attrs=["bold"]),colored(received.get("trip", ""), 'white'), colored(received["nick"], "cyan"), received["text"]))
+                    print("{}|[{}] {}".format(received.get("trip", ""), 
+                                              colored_nick, received["text"]))
+
+                else:                   
+                    print("{}{}|[{}] {}".format(you_text, 
+                                                received.get("trip", ""),
+                                                colored_nick, received["text"]))
 
             elif received["cmd"] == "onlineAdd":
                 self.online_users.append(received["nick"])
-                print("|{} joined (Trip: {})".format(colored(received["nick"], 'cyan'), colored(received.get("trip", ""), 'white')))
+                print("|" +
+                      color_list.GREEN +
+                      "{} joined (Trip: {})".format(received["nick"],
+                                                     received.get("trip", "")) + 
+                      color_list.END)
                     
             elif received["cmd"] == "onlineRemove":                
                 self.online_users.remove(received["nick"])
-                print("|{} left".format(colored(received["nick"], 'cyan')))
+                print("|" +
+                      color_list.GREEN +
+                      "{} left".format(received["nick"]) +
+                      color_list.END)
             
             elif received["cmd"] == "onlineSet":                
                 for nick in received["nicks"]:                    
                     self.online_users.append(nick)
             
             elif received["cmd"] == "emote":
+                colored_emote = color_list.GREEN + received["text"] + color_list.END
+
                 if received["nick"] != self.nick:
-                    print("{}| {}".format(colored(received.get("trip", ""), 'white'), received["text"]))                    
+                    print("{}| {}".format(received.get("trip", ""),
+                                          colored_emote))                    
                 else:
-                    print("{}{}| {}".format(colored("you:","red", attrs=["bold"]),colored(received.get("trip", ""), 'white'), received["text"]))
+                    print("{}{}| {}".format(you_text, 
+                                            received.get("trip", ""),
+                                            colored_emote))
                     
             elif received["cmd"] == "info" and received.get("type") is not None and received.get("type") == "whisper": 
-                print("{}| {}".format(colored(received.get("trip", ""),'white'), received["text"]))
+                colored_whisper = color_list.YELLOW + received["text"] + color_list.END
+                print("{}| {}".format(received.get("trip", ""),
+                                      colored_whisper))
                 
                    
                                 
@@ -86,15 +109,26 @@ class Client:
                 print("\n\nOnline users:\n{}\n\n".format("\n".join(map(str, self.online_users))))
 
 
+
+class color_list:
+
+    CYAN = '\033[1;36;48m'
+    GREEN = '\033[1;32;48m'
+    YELLOW = '\033[1;33;48m'
+    RED = '\033[1;31;48m'
+    END = '\033[1;37;0m'
+   
+
                 
 if __name__ == "__main__":
-    channel_to_join = input("Channel:")
-    nick_to_use = input("Nick:")
+    channel_to_join = input("Channel: ")
+    nick_to_use = input("Nick: ")
+    password = input("Password(optional): ")
     client = Client(nick=nick_to_use,
+                    password=password,
                     channel=channel_to_join,
                     target_websocket="wss://hack.chat/chat-ws")
     colorama.init()
     client.thread_main.start()
     client.thread_ping.start()
     client.thread_input.start()
-
