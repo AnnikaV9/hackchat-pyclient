3e0d2197e14c975a6ef19ea7d285a21465eff123
V9 <68383195+AnnikaV9@users.noreply.github.com>
2020-12-11 09:40:21 +0800

    switched to tkinter gui

--- hcclient/__main__.py
+++ hcclient/__main__.py
@@ -1,45 +1,45 @@
 # No copyright, licenses or whatever
 # Edit 'target_websocket' within the Client class to access personal servers
 
-
-
 import json
 import threading
 import websocket
-import colorama
+import tkinter as tk
+from tkinter import *
 from os import system, name
 from datetime import datetime
 from time import sleep
 
 
 
-colorama.init()
-
-
-
 class Client:
-     
-        
-    def __init__(self, nick, password, channel="programming", target_websocket="wss://hack.chat/chat-ws"):  
+
+
+    def __init__(self, root, nick, password, channel="programming", target_websocket="wss://hack.chat/chat-ws"):
+        self.main_window = tk.Frame(root, height=50, width=100)
+        self.main_window.pack(fill=Y, expand=True)
+        self.scrollbar = Scrollbar(self.main_window)
+        self.output_box = tk.Text(self.main_window, height=30, width=100, yscrollcommand=self.scrollbar.set, bg='black', fg='white')
+        self.output_box.config(state='disabled')
+        self.output_box.bind("<Button>", lambda event: self.output_box.focus_set())
+        self.input_box = tk.Text(self.main_window, height=1, width=102, bg='white', fg='black')
+        self.input_box.config(wrap='none')
+        self.input_box.pack(side='bottom')
+        self.scrollbar.config(command=self.output_box.yview)
+        self.scrollbar.pack(side=RIGHT, fill=Y)
+        self.output_box.pack(side='top')
         self.nick = nick
         self.channel = channel
         self.online_users = []
         self.full_nick = "{}#{}".format(nick, password)
         self.ws = websocket.create_connection(target_websocket)
         self.ws.send(json.dumps({"cmd": "join", "channel": channel, "nick": self.full_nick}))
+        self.refresh_display(text="You are now connected to channel: {}\nType '/help' for a list of commands you can use with this client\n\n".format(channel))
+        self.thread_ping = threading.Thread(target=self.ping_thread, daemon=True)
+        self.thread_input = threading.Thread(target=self.input_thread, daemon=True)
+        self.thread_main = threading.Thread(target=self.main_thread, daemon=True)
 
-        system('cls' if name == 'nt' else 'clear')
-
-        print("You are now connected to the channel: {}".format(color_list.RED +
-                                                                    channel +
-                                                                    color_list.END))
-        print("Type '/list' to see who's online.\n\n".format(self.online_users))
-
-        self.thread_ping = threading.Thread(target=self.ping_thread)
-        self.thread_input = threading.Thread(target=self.input_thread)
-        self.thread_main = threading.Thread(target=self.main_thread)
-
-
+    
     def main_thread(self):        
         
         while True:            
@@ -53,22 +53,11 @@ class Client:
 
                 else:
                     tripcode = received.get("trip", "")
-
-                colored_trip = color_list.WHITE + tripcode + color_list.END
-
-                if received["nick"] != self.nick:
-                    colored_nick = color_list.CYAN + received["nick"] + color_list.END
-                    print("{}|{}|[{}] {}".format(packet_receive_time,
-                                                 colored_trip,
-                                                 colored_nick,
-                                                 received["text"]))
-
-                else:                   
-                    colored_nick = color_list.RED + received["nick"] + color_list.END
-                    print("{}|{}|[{}] {}".format(packet_receive_time,
-                                                 colored_trip,
-                                                 colored_nick,
-                                                 received["text"]))
+                text_to_print = "{}|{}|[{}] {}".format(packet_receive_time,
+                                                       tripcode,
+                                                       received["nick"],
+                                                       received["text"])
+                self.refresh_display(text=text_to_print)
 
             elif received["cmd"] == "onlineAdd":
 
@@ -78,12 +67,11 @@ class Client:
                 else:
                     tripcode = received.get("trip", "")
                 
-                colored_trip = color_list.WHITE + tripcode + color_list.END
                 self.online_users.append(received["nick"])
-                print("{}|{}|".format(packet_receive_time, colored_trip) +
-                      color_list.GREEN +
-                      "{} joined".format(received["nick"]) + 
-                      color_list.END)
+                text_to_print = "{}|{}|{} joined".format(packet_receive_time,
+                                                         tripcode,
+                                                         received["nick"])
+                self.refresh_display(text=text_to_print)
                     
             elif received["cmd"] == "onlineRemove":                
 
@@ -93,12 +81,11 @@ class Client:
                 else:
                     tripcode = received.get("trip", "")
                 
-                colored_trip = color_list.WHITE + tripcode + color_list.END
                 self.online_users.remove(received["nick"])
-                print("{}|{}|".format(packet_receive_time, colored_trip) +
-                      color_list.GREEN +
-                      "{} left".format(received["nick"]) +
-                      color_list.END)
+                text_to_print = "{}|{}|{} left".format(packet_receive_time,
+                                                       tripcode,
+                                                       received["nick"])
+                self.refresh_display(text=text_to_print)
             
             elif received["cmd"] == "onlineSet":                
 
@@ -113,12 +100,10 @@ class Client:
                 else:
                     tripcode = received.get("trip", "")
 
-                colored_emote = color_list.GREEN + received["text"] + color_list.END
-                colored_trip = color_list.WHITE + tripcode + color_list.END
-
-                print("{}|{}|{}".format(packet_receive_time,
-                                        colored_trip,
-                                        colored_emote))                    
+                text_to_print = "{}|{}|{}".format(packet_receive_time,
+                                                  tripcode,
+                                                  received["text"])
+                self.refresh_display(text=text_to_print)
                     
             elif received["cmd"] == "info" and received.get("type") is not None and received.get("type") == "whisper": 
 
@@ -128,32 +113,27 @@ class Client:
                 else:
                     tripcode = received.get("trip", "")
                     
-                colored_whisper = color_list.YELLOW + received["text"] + color_list.END
-                colored_trip = color_list.WHITE + tripcode + color_list.END
-                print("{}|{}|{}".format(packet_receive_time,
-                                        colored_trip,
-                                        colored_whisper))
+                text_to_print = "{}|{}|{}".format(packet_receive_time,
+                                                  tripcode,
+                                                  received["text"])
+                self.refresh_display(text=text_to_print)
+
             
             elif received["cmd"] == "info":
-                if len(received.get("trip", "")) < 6:
-                    tripcode = "NOTRIP"
-
-                else:
-                    tripcode = received.get("trip", "")
-                colored_info = color_list.YELLOW + received["text"] + color_list.END
-                colored_trip = color_list.WHITE + "SYSTEM" + color_list.END
-                print("{}|{}|{}".format(packet_receive_time,
-                                        colored_trip,
-                                        colored_info))
-            
+ 
+                text_to_print = "{}|{}|{}".format(packet_receive_time,
+                                                  "SYSTEM",
+                                                  received["text"])
+                self.refresh_display(text=text_to_print)
+ 
             elif received["cmd"] == "warn":
-                colored_warn = color_list.YELLOW + received["text"] + color_list.END
-                colored_trip = color_list.WHITE + "!WARN!" + color_list.END
-                print("{}|{}|{}".format(packet_receive_time,
-                                        colored_trip,
-                                        colored_warn))
+
+                text_to_print = "{}|{}|{}".format(packet_receive_time,
+                                                     "!WARN!",
+                                                     received["text"])
+                self.refresh_display(text=text_to_print)
+
                    
-                                
     def ping_thread(self):        
 
         while self.ws.connected:            
@@ -162,39 +142,63 @@ class Client:
 
 
     def input_thread(self):        
+         self.input_box.bind('<Return>', lambda _: self.send_input())
+
         
-        while self.ws.connected:
-                        
-            message = str(input())
+    def refresh_display(self, text):
+        self.output_box.config(state='normal')
+        self.output_box.insert('end', text+"\n")
+        self.output_box.config(state='disabled')
+        self.output_box.yview_moveto( 1 ) 
 
-            if message == "/list":
-                print("\n\nOnline users:\n{}\n\n".format("\n".join(map(str, self.online_users))))
-             
-            else:
-                self.ws.send(json.dumps({"cmd": "chat", "text": message}))                       
+                    
+    def send_input(self):
+        message = self.input_box.get("1.0", "end-1c")
+        listed = message.split()
+        message = ' '.join(listed)
+        message = message.replace("/n/", "\n")
+        check = ''.join(listed)
 
+        if message.split()[0] == "/raw":
+            split_message = message.split()
+            split_message.pop(0)
+            to_send = ' '.join(split_message)
+            self.input_box.delete("1.0", END)
 
+            try:
+                json_to_send = json.loads(to_send)
+                self.ws.send(json.dumps(json_to_send))
 
+            except:
+                self.refresh_display(text="\nError sending json")
+        
+        elif message == "/list":
+            user_list = "\n\nChannel: {}\nOnline users:\n{}\n\n".format(channel, "\n".join(map(str, self.online_users)))
+            self.input_box.delete("1.0", END)
+            self.refresh_display(text=user_list)
 
-class color_list:
+        else:
+            self.input_box.delete("1.0", END)
+            self.ws.send(json.dumps({"cmd": "chat", "text": message}))     
 
-    CYAN = '\033[1;36;48m'
-    GREEN = '\033[1;32;48m'
-    YELLOW = '\033[1;33;48m'
-    RED = '\033[1;31;48m'
-    WHITE = '\33[37m'
-    END = '\033[1;37;0m'
+            if message == "/help":
+                extra_text = "\n\nAll '/n/' will be converted into linebreaks\n\nRaw json packets can be sent with '/raw'\nUsage: /raw <json>\n\nUser list can be viewed with '/list'\nUsage: /list\n\n"
+                self.refresh_display(text=extra_text)
+        
    
-
-                
 if __name__ == "__main__":
-    channel_to_join = input("Channel: ")
-    nick_to_use = input("Nick: ")
+    channel = input("Channel: ")
+    nick = input("Nick: ")
     password = input("Password(optional): ")
-    client = Client(nick=nick_to_use,
+    root = tk.Tk()
+    root.title("hcclient")
+    client = Client(root=root,
+                    nick=nick,
                     password=password,
-                    channel=channel_to_join,
+                    channel=channel,
                     target_websocket="wss://hack.chat/chat-ws")
+    root.resizable(False, False)
     client.thread_main.start()
     client.thread_ping.start()
     client.thread_input.start()
+    root.mainloop()
